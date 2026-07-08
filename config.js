0ddfb9e8910a55dc9c980c506684e17e27e38bc1
scottschrum79-design <scottschrum79@gmail.com>
2026-07-08 13:11:40 -0400

    Configure creator page password

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 window.TEAMSIGNUPS_CONFIG = {
   // Paste your deployed Google Apps Script Web App URL here.
   // Leave blank to use local/server storage at /api/events.
-  googleScriptUrl: "https://script.google.com/macros/s/AKfycby6dRRN71zCkZD3hum51RkhgfqZn6cgOgCFom-DPE4OAC6iMzd6hFOF5Gb8ut5DzlyS/exec"
+  googleScriptUrl: "https://script.google.com/macros/s/AKfycby6dRRN71zCkZD3hum51RkhgfqZn6cgOgCFom-DPE4OAC6iMzd6hFOF5Gb8ut5DzlyS/exec",
+
+  // Password for the creator page. This is a lightweight static-site gate.
+  creatorPassword: "CVsoccer2026!"
 };
