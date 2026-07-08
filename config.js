2702f26030754b5747d04813fc050c833d759f1f
scottschrum79-design <scottschrum79@gmail.com>
2026-07-08 14:25:13 -0400

    Update Google Apps Script endpoint

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.TEAMSIGNUPS_CONFIG = {
   // Paste your deployed Google Apps Script Web App URL here.
   // Leave blank to use local/server storage at /api/events.
-  googleScriptUrl: "https://script.google.com/macros/s/AKfycby6dRRN71zCkZD3hum51RkhgfqZn6cgOgCFom-DPE4OAC6iMzd6hFOF5Gb8ut5DzlyS/exec",
+  googleScriptUrl: "https://script.google.com/macros/s/AKfycbwicKv13R4Nxsx6G_-9eTNZSQqIuEXvNO7Zit3gI2sgUAlSYNVZ4Czd-2feDMRCw6bJ/exec",
 
   // Password for the creator page. This is a lightweight static-site gate.
   creatorPassword: "CVsoccer2026!"
